e33e9d2a3421cde38e4805dc650edc8b2cd0ce10
Adrien <garbani.adrien@yahoo.fr>
2024-07-11 01:57:12 +0200

    file

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,29 @@
-#include <print>
 #include <iostream>
+
+#include <format>
+
+#include <filesystem>
+namespace fs = std::filesystem;
+
+#include <chrono>
+namespace ch = std::chrono;
 
 #include <boost/program_options.hpp>
 namespace po = boost::program_options;
 
-int main(int argc, char** argv)
+struct Config
+{
+    fs::path input_dir;
+    fs::path output_dir;
+};
+
+Config parse_arguments(int argc, char** argv)
 {
     po::options_description desc("Allowed options");
     desc.add_options()
-        ("help", "produce help message")
-        ("input,i", po::value<std::string>(), "input directory")
-        ("output,o", po::value<std::string>(), "output directory")
+        ("help,h", "produce help message")
+        ("input,i", po::value<fs::path>(), "input directory")
+        ("output,o", po::value<fs::path>(), "output directory")
     ;
 
     po::variables_map vm;
@@ -19,8 +32,78 @@ int main(int argc, char** argv)
 
     if (vm.count("help")) {
         std::cout << desc << std::endl;
-        return 0;
+        std::exit(0);
     }
 
-    std::println("Hello world !");
+    Config config;
+
+    if(!vm.count("input"))
+    {
+        throw std::invalid_argument("missing input");
+    }
+
+    if(!vm.count("output"))
+    {
+        throw std::invalid_argument("missing output");
+    }
+
+    config.input_dir = vm["input"].as<fs::path>();
+    config.output_dir = vm["output"].as<fs::path>();
+
+    if(!fs::is_directory(config.input_dir))
+    {
+        throw std::invalid_argument("invalid input");
+    }
+
+    if(!fs::is_directory(config.output_dir))
+    {
+        throw std::invalid_argument("invalid output");
+    }
+
+    return config;
+}
+
+void run(const Config & config)
+{
+    const ch::time_zone* tz = ch::current_zone();
+
+    for(const fs::path & in : fs::recursive_directory_iterator(config.input_dir))
+    {
+        const fs::path rel = fs::relative(in, config.input_dir);
+        // fs::path out = config.output_dir / rel;
+        // out.remove_filename();
+        // fs::create_directories(out);
+        if(fs::is_regular_file(in))
+        {
+            fs::file_time_type file_time = fs::last_write_time(in);
+            ch::sys_time sys_time = ch::clock_cast<ch::system_clock>(file_time);
+            ch::local_time local_time = tz->to_local(sys_time);
+
+            std::string date = std::format("{:%Y.%m.%d-%H.%M.%OS}_", local_time);
+
+            fs::path out = config.output_dir / date;
+            out += in.filename();
+
+            fs::copy_file(in, out);
+        }
+    }
+}
+
+int main(int argc, char** argv)
+{
+    try
+    {
+        Config config = parse_arguments(argc, argv);
+
+        std::cout << "input=" << config.input_dir << std::endl;
+        std::cout << "output=" << config.output_dir << std::endl;
+
+        run(config);
+    }
+    catch(std::exception & e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
 }
